c9945f4d49475729d4cb8b0786c011f890c37173
AniketJoshi01 <aniketnandanj@gmail.com>
2018-09-01 10:00:15 -0700

    Add files via upload

---
+++ chat-text/chat-text.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ChatTextPage } from './chat-text';
+// messagelist.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { OktaAuthService } from '@okta/okta-angular';
+import 'rxjs/Rx';
+
+interface Message {
+   date: String,
+   text: String
+}
+
+@Component({
+  template: `
+    <div *ngIf="messages.length">
+      <li *ngFor="let message of messages">{{message.message}}</li>
+    </div>
+  `
+})
+export class MessageListComponent implements OnInit{
+  messages: Array<Message> [];
+
+  constructor(private oktaAuth: OktaAuthService, private http: Http) {
+    this.messages = [];
+  }
+
+  async ngOnInit() {
+    const accessToken = await this.oktaAuth.getAccessToken();
+    const headers = new Headers({
+      Authorization: 'Bearer ' + accessToken
+    });
+    // Make request
+    this.http.get(
+      'http://localhost:{serverPort}/api/messages',
+      new RequestOptions({ headers: headers })
+    )
+    .map(res => res.json())
+    .subscribe((messages: Array<Message>) => messages.forEach(message => this.messages.push(message)));
+  }
+}
+
+@NgModule({
+  declarations: [
+    ChatTextPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ChatTextPage),
+  ],
+})
+export class ChatTextPageModule {}
---
+++ chat-text/chat-text.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+/**
+ * Generated class for the ChatTextPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-chat-text',
+  templateUrl: 'chat-text.html',
+})
+export class ChatTextPage {
+  messages = [];
+  nickname = '';
+  message = '';
+
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController, public navParams: NavParams, private socket: Socket/) {
+
+    this.nickname = this.navParams.get('nickname');
+
+    this.getMessages().subscribe(message => {
+      this.messages.push(message);
+    });
+
+    this.getUsers().subscribe(data => {
+      let user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left:' + user);
+      } else {
+        this.showToast('User joined:' + user);
+      }
+    });
+  }
+
+  getUsers() {
+    let observable = new Observable(observer => {
+      this.socket.on('users-changed', data => {
+        observer.next(data);
+      })
+    });
+    return observable;
+  }
+
+  sendMessage() {
+    this.socket.emit('add-message', { text: this.message});
+    this.message = '';
+  }
+
+  getMessages() {
+    let observable = new Observable(observer => {
+      this.socket.on('message', data => {
+        observer.next(data);
+      })
+    });
+    return observable;
+  }
+
+  ionViewWillLeave() {
+    this.socket.disconnect();
+
+  }
+
+  showToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+}
